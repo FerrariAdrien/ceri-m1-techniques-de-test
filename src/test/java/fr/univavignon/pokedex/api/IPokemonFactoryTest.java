3fc058154cf74f44d620a073a616ba2c37abd9d0
gadrew <gadrew157@gmail.com>
2024-02-13 10:40:21 +0100

    TP3 episode 1 ajout test pour IPokemonFactoryTest

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -1,8 +1,13 @@
 package fr.univavignon.pokedex.api;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class  IPokemonFactoryTest {
 
@@ -11,7 +16,33 @@ public class  IPokemonFactoryTest {
 
     @Before
     public void setUp(){
+        iPokemonFactory = mock(IPokemonFactory.class);
+        when(iPokemonFactory.createPokemon(1, 100, 100, 100, 100)).thenReturn(new Pokemon(1, "Bulbasaur", 100, 100, 100, 100, 100, 100, 100, 100));
 
     }
 
+    @Test
+    public void testGetCp(){
+        assertEquals(iPokemonFactory.createPokemon(1, 100, 100, 100, 100).getCp(), 100);
+    }
+
+    @Test
+    public void testGetHp(){
+        assertEquals(iPokemonFactory.createPokemon(1, 100, 100, 100, 100).getHp(), 100);
+    }
+
+    @Test
+    public void testGetDust(){
+        assertEquals(iPokemonFactory.createPokemon(1, 100, 100, 100, 100).getDust(), 100);
+    }
+
+    @Test
+    public void testGetCandy(){
+        assertEquals(iPokemonFactory.createPokemon(1, 100, 100, 100, 100).getCandy(), 100);
+    }
+
+    @Test
+    public void testGetIv(){
+        assertEquals(iPokemonFactory.createPokemon(1, 100, 100, 100, 100).getIv(), 100);
+    }
 }
