1f4551353424fa109b5e3cceafc4d06723959d20
gadrew <gadrew157@gmail.com>
2024-02-13 11:13:40 +0100

    TP3 episode 1 ajout des tests pour IPokedexTest

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,12 +1,35 @@
 package fr.univavignon.pokedex.api;
 
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static org.mockito.Mockito.*;
 public class IPokedexFactoryTest {
 
+/*
+    @Mock
+    private static IPokedexFactory iPokedexFactory;
 
+    @Mock
+    private static IPokemonMetadataProvider metadataProvider;
 
+    @Mock
+    private static IPokemonFactory pokemonFactory;
 
+    @Before
+    public void setUp(){
+        // Créer un mock de IPokedexFactory
+        iPokedexFactory = mock(IPokedexFactory.class);
+
+        // Créer des mocks pour les arguments de la méthode createPokedex
+        metadataProvider = mock(IPokemonMetadataProvider.class);
+        pokemonFactory = mock(IPokemonFactory.class);
+
+        // Définir le comportement du mock
+        when(iPokedexFactory.createPokedex(metadataProvider, pokemonFactory)).thenReturn(1);
+    }
+
+*/
 }
--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -1,6 +1,51 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class IPokedexTest {
 
+    @Mock
+    private static IPokedex iPokedex;
+
+    private Pokemon pokemon;
+    @Before
+    public void setUp() throws PokedexException {
+        iPokedex = mock(IPokedex.class);
+        pokemon = new Pokemon(1, "Bulbasaur", 100, 100, 100, 100, 100, 100, 100, 100);
+        when(iPokedex.addPokemon(any(Pokemon.class))).thenReturn(pokemon.getIndex());
+        when(iPokedex.getPokemon(1)).thenReturn(pokemon);
+        when(iPokedex.size()).thenReturn(5);
+        when(iPokedex.getPokemons()).thenReturn(new ArrayList<Pokemon>());
+        when(iPokedex.getPokemons(null)).thenReturn(new ArrayList<Pokemon>());
+    }
+
+    @Test
+    public void testAddPokemon() throws PokedexException {
+        assertEquals(iPokedex.addPokemon(pokemon), pokemon.getIndex());
+    }
+
+    @Test
+    public void testGetPokemon() throws PokedexException {
+        assertEquals(iPokedex.getPokemon(1), pokemon);
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(iPokedex.size(), 5);
+    }
+
+    @Test
+    public void testGetPokemons() {
+        assertEquals(iPokedex.getPokemons(), new ArrayList<Pokemon>());
+    }
 
 }
