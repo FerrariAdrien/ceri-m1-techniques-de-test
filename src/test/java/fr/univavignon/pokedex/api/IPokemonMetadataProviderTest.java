4cdcac478b7915c4ab1446b0974718e0734bad40
gadrew <gadrew157@gmail.com>
2024-02-13 09:09:55 +0100

    Fin TP2

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,5 +1,44 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
+
 public class IPokemonMetadataProviderTest {
+
+    @Mock
+    private static PokemonMetadata pokemonMetadata;
+
+    @BeforeAll
+    public static void setUp(){
+        pokemonMetadata = new PokemonMetadata(1,"slafleche",999,1,543);
+    }
+
+    @Test
+    public void testIndex(){
+        assertEquals(pokemonMetadata.getIndex(),1);
+    }
+
+    @Test
+    public void testName(){
+        assertEquals(pokemonMetadata.getName(),"slafleche");
+    }
+
+    @Test
+    public void testAttack(){
+        assertEquals(pokemonMetadata.getAttack(),999);
+    }
+
+    @Test
+    public void testDefense(){
+        assertEquals(pokemonMetadata.getDefense(),1);
+    }
+
+    @Test
+    public void testStamina(){
+        assertEquals(pokemonMetadata.getStamina(),543);
+    }
 
 }
