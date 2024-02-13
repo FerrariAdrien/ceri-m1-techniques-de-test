af6553c7f9688eeadc186d59e22abf9a81260571
gadrew <gadrew157@gmail.com>
2024-02-13 13:47:11 +0100

    Fin TP3

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -8,7 +8,7 @@ import org.mockito.Mock;
 import static org.mockito.Mockito.*;
 public class IPokedexFactoryTest {
 
-/*
+
     @Mock
     private static IPokedexFactory iPokedexFactory;
 
@@ -27,9 +27,25 @@ public class IPokedexFactoryTest {
         metadataProvider = mock(IPokemonMetadataProvider.class);
         pokemonFactory = mock(IPokemonFactory.class);
 
-        // Définir le comportement du mock
-        when(iPokedexFactory.createPokedex(metadataProvider, pokemonFactory)).thenReturn(1);
     }
 
-*/
+    @Test
+    public void testCreatePokedex(){
+        // Créer un mock de IPokedex
+        IPokedex pokedex = mock(IPokedex.class);
+
+        // Définir le comportement du mock de IPokedexFactory
+        when(iPokedexFactory.createPokedex(metadataProvider, pokemonFactory)).thenReturn(pokedex);
+
+        // Appeler la méthode createPokedex
+        IPokedex pokedex2 = iPokedexFactory.createPokedex(metadataProvider, pokemonFactory);
+
+        // Vérifier que la méthode createPokedex a été appelée avec les bons arguments
+        verify(iPokedexFactory).createPokedex(metadataProvider, pokemonFactory);
+
+        // Vérifier que le résultat de la méthode est bien le mock de IPokedex
+        assert(pokedex2 == pokedex);
+    }
+
+
 }
