e4d1c7967aff0466e7a107070326cc881c5db9a0
gadrew <gadrew157@gmail.com>
2024-03-12 10:50:33 +0100

    maj check style

--- src/main/java/fr/univavignon/pokedex/api/Pokedex.java
+++ src/main/java/fr/univavignon/pokedex/api/Pokedex.java
@@ -9,10 +9,15 @@ public class Pokedex implements IPokedex{
 
 
 
-
+    /**
+     * Liste des pokemons.
+     */
     Pokedex(){
         pokemons = new ArrayList<>();
     }
+    /**
+     * Constructeur de la classe Pokedex.
+     */
     public List<Pokemon> pokemons;
     @Override
     public int size() {
@@ -20,25 +25,36 @@ public class Pokedex implements IPokedex{
         return pokemons.size();
     }
 
+    /**
+     * Ajoute un pokemon à la liste.
+     *
+     * @param pokemon le pokemon à ajouter
+     */
     @Override
-    public int addPokemon(Pokemon pokemon) {
+    public int addPokemon(final Pokemon pokemon) {
 
         pokemons.add(pokemon);
-        System.out.println(pokemons.size() -1);
+
         return pokemons.size() - 1;
     }
 
+    /**
+     * Retourne le nombre de pokemons dans la liste.
+     *
+     * @return le nombre de pokemons
+     */
     @Override
-    public Pokemon getPokemon(int id) throws PokedexException {
-        if(id < pokemons.size())
-            return pokemons.get(id);
-        else
+    public Pokemon getPokemon(final int id) throws PokedexException {
+        if (id < this.pokemons.size()) {
+            return this.pokemons.get(id);
+        } else {
             throw new PokedexException("Invalid index");
+        }
     }
 
     @Override
     public List<Pokemon> getPokemons() {
-        return pokemons;
+        return this.pokemons;
     }
 
     /**
@@ -48,28 +64,30 @@ public class Pokedex implements IPokedex{
      * @param order Comparator instance used for sorting the created view.
      * @return Sorted unmodifiable list of all pokemons.
      */
+
     @Override
-    public List<Pokemon> getPokemons(Comparator<Pokemon> order) {
-        List<Pokemon> sortedPokemons = new ArrayList<>(pokemons);
+    public List<Pokemon> getPokemons(final Comparator<Pokemon> order) {
+        List<Pokemon> sortedPokemons = new ArrayList<>(this.pokemons);
         Collections.sort(sortedPokemons, order);
         return Collections.unmodifiableList(sortedPokemons);
     }
 
-
     @Override
-    public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) throws PokedexException {
+    public Pokemon createPokemon(final int index, final int cp, final int hp, final int dust, final int candy) throws PokedexException {
         PokemonMetadata pokemonMetadata = getPokemonMetadata(index);
-        if(pokemonMetadata == null)
+        if (pokemonMetadata == null) {
             throw new PokedexException("Invalid index");
-        pokemons.add(new  Pokemon(index, pokemonMetadata.getName(), pokemonMetadata.getAttack(), pokemonMetadata.getDefense(), pokemonMetadata.getStamina(),cp,hp,dust, candy,5));
-        return pokemons.get(pokemons.size() - 1);
+        }
+        this.pokemons.add(new Pokemon(index, pokemonMetadata.getName(), pokemonMetadata.getAttack(), pokemonMetadata.getDefense(), pokemonMetadata.getStamina(), cp, hp, dust, candy, 5));
+        return this.pokemons.get(this.pokemons.size() - 1);
     }
 
     @Override
-    public PokemonMetadata getPokemonMetadata(int index) throws PokedexException {
-        for(int i = 0; i < pokemons.size(); i++){
-            if(pokemons.get(i).getIndex() == index)
-                return new PokemonMetadata(index, pokemons.get(i).getName(), pokemons.get(i).getAttack(),pokemons.get(i).getDefense(), pokemons.get(i).getStamina());
+    public PokemonMetadata getPokemonMetadata(final int index) throws PokedexException {
+        for (int i = 0; i < this.pokemons.size(); i++) {
+            if (this.pokemons.get(i).getIndex() == index) {
+                return new PokemonMetadata(index, this.pokemons.get(i).getName(), this.pokemons.get(i).getAttack(), this.pokemons.get(i).getDefense(), this.pokemons.get(i).getStamina());
+            }
         }
         return null;
     }
